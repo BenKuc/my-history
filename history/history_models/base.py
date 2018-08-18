b2624d392b73020e89de14c5fc463c4ff8bbb3c8
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-18 20:25:17 +0200

    added more strcuture in TODOs

--- history/history_models/base.py
+++ history/history_models/base.py
@@ -1,61 +1,72 @@
-from django.db.models import Model as DjangoModel
+from django.conf import settings
+from django.db import models
+
+# TODO: move these to constants
+# TODO: more information by model-cls and so on
+ERROR_TYPE_FIELDS = "Parameter fields of history decorator " \
+                    "must be a list or tuple."
+ERROR_TYPE_MODEL = "The registered class must be a subtype of " \
+                   "django.db.models.Model"
 
 
-# TODO: just include this in the code and put it into installed-apps as: django_history
+def get_history_model_name(model):
+    scheme = settings.MY_HISTORY.MODEL_NAMING_SCHEME
+    return scheme.format(name=model.__name__)
 
-# TODO: mapping from models to already registered history-models to use tables efficiently
+
+def get_app_label():
+    return settings.MY_HISTORY.APP_NAME
 
 
-def get_history_fields(model, fields):
-    pass
+def get_history_model_dict(model, fields):
     # TODO: get related models to be processed by history
+    history_dict = {}
+    return history_dict
 
 
-def get_history_model_name(model_name):
-    # TODO: involve app name to avoid conflicts
+def get_model_meta_dict(model):
     pass
 
 
-def history(fields='__all__', differences=False, db_alias='default', history_model_name=''):
+def history(fields='__all__', differences=False):
     """
-
     :param fields: fields that should be tracked
     :param differences: boolean indicating whether differences shall be tracked
-    :param db_alias: connection string to the database where the history-table should be created
-    :param history_model_name: name of the history model to be used
-    :return:
     """
-    # TODO: create model that is checked -> do you get the model or the class definition?
 
     if fields == '__all__':
         fields = []
-    assert isinstance(fields, (list, tuple)), "Parameter fields of history decorator must be a list or tuple."
+    assert isinstance(fields, (list, tuple)), ERROR_TYPE_FIELDS
 
-    def create_history_model(ModelClass: DjangoModel):
-        # TODO: set custom managers on the ModelClass
-        assert isinstance(ModelClass, DjangoModel), "The registered class must be a subtype of django.db.models.Model"
+    # TODO: get fields and related-history-models -> make Queue!
+    # TODO: get history-name by scheme of settings
+    # TODO: Meta-class -> db_table name! and app_label!
+    # TODO: custom managers on cls!
 
-        # ???: This might not work -> when does this code run?
-        model_fields = [f for f in ModelClass._meta.get_fields() if f in fields]
+    def create_history_model(model):
+        assert isinstance(model, models.Model), ERROR_TYPE_MODEL
+
+        # ???: do we need the class to be prepared here?
+        # TODO: connect to class_prepared for each one of it
+        # TODO: also make a centralized place where duplicate registries are
+        #       prevented (because of following relations)
+        model_fields = [f for f in model._meta.get_fields() if f.name in fields]
         relations = {f.name: f for f in model_fields if f.is_relation}
 
-        registered_models = ModelTracker.objects.select_related('model').all()
-        # TODO: follow relations that should be set up exactly the same way -> get reverse relations (lazily?)
+        # TODO: follow relations that should be set up exactly the same way
         for rel in relations:
             # TODO: check for related_name and if it is in relations!
-
-            # TODO: register all of them in the db?
-            if rel.related_model in registered_models:
-                pass
-            else:
-                # TODO: create sub history_model and register it
-                pass
+            # TODO: create sub_history_models
+            pass
 
         # TODO: register and create the main model here
         # TODO: how to set which app it belongs to?
-        history_model = type(history_model_name, (DjangoModel,), dict={})
+
+        # TODO: get correct bases -> multi-table inheritance
+        type(
+            name=get_history_model_name(model),
+            bases=(models.Model, ),
+            dict=get_history_model_dict(model),
+        )
 
     return create_history_model
-
-
-# TODO: handle different inheritance structures comming with Python (multi-table-inheritance)
