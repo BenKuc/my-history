07da2adcd2d47de4840c45f099621fae32003756
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-19 14:35:11 +0200

    moved meesages to constants and added more structure

--- history/history_models/register.py
+++ history/history_models/register.py
@@ -3,12 +3,7 @@ from collections import namedtuple
 from django.conf import settings
 from django.db import models
 
-# TODO: move these to constants
-# TODO: more information by model-cls and so on
-ERROR_TYPE_FIELDS = "Parameter fields of history decorator " \
-                    "must be a list or tuple."
-ERROR_TYPE_MODEL = "The registered class must be a subtype of " \
-                   "django.db.models.Model"
+from .constants import ERROR_MESSAGES
 
 HistoryConfig = namedtuple(
     typename='HistoryConfig',
@@ -29,14 +24,14 @@ def history(fields='__all__', track_diffs=False, extra_fields=None):
 
     if fields == '__all__':
         fields = []
-    assert isinstance(fields, (list, tuple)), ERROR_TYPE_FIELDS
+    assert isinstance(fields, (list, tuple)), ERROR_MESSAGES['TYPE_FIELDS']
 
     # TODO: inspect those + add global_extra_fields -> django-checks?
     if extra_fields:
         pass
 
     def set_history_on_model(model):
-        assert issubclass(model, models.Model), ERROR_TYPE_MODEL
+        assert issubclass(model, models.Model), ERROR_MESSAGES['TYPE_MODEL']
         if model not in TRACKED_MODELS:
             TRACKED_MODELS[model] = HistoryConfig(
                 field_names=fields,
@@ -51,16 +46,60 @@ def history(fields='__all__', track_diffs=False, extra_fields=None):
     return set_history_on_model
 
 
-# TODO: get fields and related-history-models. ->
-    #  TODO: also connect sub-stuff to class_prepared!
-    # TODO: get history-name by scheme of settings.
-    # TODO: Meta-class -> db_table name! and app_label!.
-    # TODO: custom managers on cls!
 def create_history_on_models():
-    # TODO: this needs to be sorted though -> so you can use all of them
-    # TODO: do this by the hand
     for model_cls, config in TRACKED_MODELS.items():
-        opts = model_cls._meta
+        creator = HistoryCreator(model_cls, config)
+        creator.create_history()
+
+
+# TODO: custom managers on cls!
+class HistoryCreator:
+
+    def __init__(self, model_cls, config):
+        self.model = model_cls
+        self.config = config
+
+    def create_history(self):
+        scheme = settings.MY_HISTORY.MODEL_NAMING_SCHEME
+        name = scheme.format(name=self.model.__name__)
+
+        bases = self.get_bases()
+
+        # app_label = settings.MY_HISTORY.APP_NAME
+
+        history_dict = self.get_dict()
+
+        # TODO: get correct bases -> multi-table inheritance
+        type(name, bases, dict=history_dict)
+
+    def get_bases(self):
+        # TODO: do this correctly (also multi-table inheritance)
+        for b in self.model.mro():
+            pass
+
+    def get_dict(self):
+        history_dict = {}
+        history_dict.update(self.get_fields())
+
+        class Meta:
+            pass
+
+        for key, val in self.get_meta_options():
+            setattr(Meta, key, val)
+
+        history_dict.update({'Meta': Meta})
+
+        return history_dict
+
+    def get_fields(self):
+        opts = self.model._meta
+        history_fields = {}
+
+        # TODO: think fo all cases here: also reverse one-to-one and
+        #       multi-table-inheritance -> set custom bulk_create e.g.
+
+        # TODO: set correct references: history.HistoryModel/SimpleReference
+        #       depending on whether related_model in TRACKED_MODELS
 
         # 1) FK and OneToOne
         for f in opts.fields:
@@ -78,55 +117,13 @@ def create_history_on_models():
         for rel in opts.related_objects:
             pass
 
+        for f in self.config.extra_fields:
+            pass
 
-def get_history_fields():
-    pass
+        return history_fields
 
-
-def get_meta_options():
-    pass
-
-
-def get_history_model_name(model):
-    scheme = settings.MY_HISTORY.MODEL_NAMING_SCHEME
-    return scheme.format(name=model.__name__)
-
-
-def get_app_label():
-    return settings.MY_HISTORY.APP_NAME
-
-
-def get_history_model_dict(model, field_names, extra_fields):
-    # TODO: get related models to be processed by history
-    # TODO: integrate global_extra_fields from settings!
-    history_dict = {}
-
-    # ???: do we need the class to be prepared here?
-    # TODO: connect to class_prepared for each one of it
-    # TODO: also make a centralized place where duplicate registries are
-    #       prevented (because of following relations)
-    model_fields = [f for f in model._meta.get_fields() if f.name in fields]
-    relations = {f.name: f for f in model_fields if f.is_relation}
-
-    # TODO: follow relations that should be set up exactly the same way
-    for rel in relations:
-        # TODO: check for related_name and if it is in relations!
-        # TODO: create sub_history_models
-        pass
-
-    # TODO: register and create the main model here
-    # TODO: how to set which app it belongs to?
-
-    # TODO: get correct bases -> multi-table inheritance
-    type(
-        name=get_history_model_name(model),
-        bases=(models.Model,),
-        dict=get_history_model_dict(model, extra_fields, field_names),
-    )
-
-    return history_dict
-
-
-def get_model_meta_dict(model):
-    # TODO: set app_label here
-    pass
+    def get_meta_options(self):
+        options = {'app_label': 'history'}
+        # TODO: look into django-model Meta docs
+        # TODO: db_table_name!
+        return options
