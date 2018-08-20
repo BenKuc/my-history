023bbd7e6c1868aea8ce22b7e6d689e4bd990935
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-21 01:32:26 +0200

    added TODOs and specified extra_fields inspection

--- history/models/register.py
+++ history/models/register.py
@@ -1,18 +1,26 @@
 from collections import namedtuple
+import inspect
 
 from django.conf import settings
+from  django.core.exceptions import ImproperlyConfigured
 from django.db import models
 
+from myhistory.settings_history import GLOBAL_TRACK_FIELDS
 from .constants import ERROR_MESSAGES
+from .models import HistoryQuerySet, ModelQuerySet, GenericRelation
+
 
 HistoryConfig = namedtuple(
     typename='HistoryConfig',
     field_names='field_names extra_fields track_diffs',
 )
 
+
 TRACKED_MODELS = {}
 
 
+# TODO: actually we want to modify the model-class before it is given to
+#       the django meta-class! -> look into order of statements
 def history(fields='__all__', track_diffs=False, extra_fields=None):
     """
     :param fields: fields that should be tracked
@@ -26,9 +34,16 @@ def history(fields='__all__', track_diffs=False, extra_fields=None):
         fields = []
     assert isinstance(fields, (list, tuple)), ERROR_MESSAGES['TYPE_FIELDS']
 
-    # TODO: inspect those + add global_extra_fields -> django-checks?
-    if extra_fields:
-        pass
+    extra = extra_fields + GLOBAL_TRACK_FIELDS
+    for _, field, method in extra.copy():
+        if not isinstance(field, models.Field):
+            # TODO: django-check as error -> for not valid fields
+            raise ImproperlyConfigured()
+        sig = inspect.signature(method)
+        # TODO: add correct error messages
+        assert set(sig.parameters.keys()) == {'instance', 'request'}, ""
+        assert sig.parameters['instance'].kind == inspect.Parameter.POSITIONAL_ONLY, ""
+        assert sig.parameters['request'].kind == inspect.Parameter.POSITIONAL_ONLY, ""
 
     def set_history_on_model(model):
         assert issubclass(model, models.Model), ERROR_MESSAGES['TYPE_MODEL']
@@ -38,10 +53,22 @@ def history(fields='__all__', track_diffs=False, extra_fields=None):
                 extra_fields=extra_fields,
                 track_diffs=track_diffs,
             )
-        # TODO: set history manager
-        # TODO: set GenericRelation to history!
+
         # TODO: wrap methods that need to send a signal
         #        -> (QuerySet.update/bulk_create)
+        # set overwritten manager
+        setattr(model, 'objects', ModelQuerySet.as_manager())
+
+        # set history manager
+        # TODO: get name from settings
+        setattr(model, 'history_objects', HistoryQuerySet.as_manager())
+
+        # TODO: this might not be possible (test + see comment above!)
+        # set extra relation
+        # TODO: related_query_name?
+        # TODO: get name from settings
+        # TODO: set this correctly
+        setattr(model, 'history', GenericRelation())
         return model
 
     return set_history_on_model
