9a880c9df1b4f755d19e9816180a4df21615f97c
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-20 14:44:05 +0200

    moved TODOs to TODO-file

--- history/apps.py
+++ history/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 from django.conf import settings
-from django.db.models.signals import class_prepared, post_save
+from django.db.models.signals import post_save
 
 # from ..receivers import save_receiver
 from django.apps.registry import apps
@@ -9,7 +9,6 @@ from .history_models.register import create_history_on_models
 
 
 class HistoryConfig(AppConfig):
-    # name = settings.MY_HISTORY['APP_NAME']
     name = 'history'
     verbose_name = settings.MY_HISTORY['VERBOSE_APP_NAME']
 
@@ -20,8 +19,4 @@ class HistoryConfig(AppConfig):
         super().import_models()
 
     def ready(self):
-        # TODO: how to get sender in here? maybe even multiple senders? -> multiple models
-        #       post_save.connect(save_receiver, sender=None)
-        # TODO: connect all signals to be connected here!
-        # TODO: test for duplicated signals
         pass
