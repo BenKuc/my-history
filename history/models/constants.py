6f8154c978c390e9208ae85b807c805936e8e12a
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-21 22:43:41 +0200

    deleted models-directory

--- history/models/constants.py
+++
@@ -1,9 +0,0 @@
-# TODO: more information by model-cls and so on
-# TODO: make a class out of it that sets variable automatically?
-# TODO: better replace by django-checks
-ERROR_MESSAGES = {
-    'TYPE_FIELDS': "Parameter fields of history decorator "
-                   "must be a list or tuple.",
-    'TYPE_MODEL': "The registered class must be a subtype of "
-                  "django.db.models.Model",
-}
