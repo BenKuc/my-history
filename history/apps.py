92fa4b9effd8dc719eb3793ed994c83c5d3e9d0d
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-22 19:27:29 +0200

    deleted ready() as we connect signals in history-creation

--- history/apps.py
+++ history/apps.py
@@ -1,20 +1,13 @@
 from django.apps import AppConfig
 from django.conf import settings
 
-# from ..receivers import save_receiver
-
-from history.models.register import create_history_on_models
-
 
 class HistoryConfig(AppConfig):
     name = 'history'
     verbose_name = settings.MY_HISTORY['VERBOSE_APP_NAME']
 
     def import_models(self):
+        # TODO: necessary? -> create history models here by TRACKED_MODELS
         # if this app is set as the last one in INSTALLED_APPS
         # all relevant models are loaded now
-        create_history_on_models()
         super().import_models()
-
-    def ready(self):
-        pass
