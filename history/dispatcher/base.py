b3828e012882f5539b45d19e96e30f4ae347416f
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-22 06:37:27 +0200

    deleted unnecessary dispatcher

--- history/dispatcher/base.py
+++
@@ -1,4 +0,0 @@
-# TODO: implement a dispatcher to bundle history functinality
-
-# TODO: corresponding to that: decorator + context manager: history.atomic -> all objects will be saved in the same history-event
-
