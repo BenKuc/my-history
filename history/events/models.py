64bd3c2b0475d1595da9171e7c3acd3ef18ed590
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-22 15:37:57 +0200

    new design decision: just have one event model

--- history/events/models.py
+++ history/events/models.py
@@ -1,75 +1,83 @@
 from django.db import models
 
 
-# TODO: complete this
 __all__ = [
-    'ObjectEvent', 'Update', 'Creation', 'Deletion',
+    'ObjectEvent',
 ]
 
 
-# TODO: this must be adjusted to differences-boolean
-#       -> field is added if True, otherwise not
-# TODO: if true -> consistency checks
-class Event(models.Model):
-    event_id = None
-    event_date = None
-    user = None
-
-    diff = None
-    trigger_method = None # TODO: choices: bulk_create, delete, update, save
-
-    # TODO: referenced pks of objects must be the same + unique with the event_id
-
-    # TODO: next and previous
-    # TODO: event and next_event -> this probably goes on the model
-
-
-class HistoryEvent(models.Model):
-    pass
-
-
-class ManagerEvent(models.Model):
-    # TODO: type + reference to object_event?
-    pass
-
-
 # TODO: consistency: same (content)type + same id -> CHECK-constraint
-# TODO: create indexes in history_date and
+# TODO: create indexes in history_date and ...
+# TODO: next and previous
+# TODO: event and next_event -> this probably goes on the model
+# TODO: check constraints for before and after
+# TODO: do check constraints that prevent saving before/after when the type
+#       is create/update -> sql
 class ObjectEvent(models.Model):
-    super_event = models.OneToOneField(ManagerEvent)
-    # TODO: related_query_name?
-    object_history = models.ForeignKey('history.ObjectHistory', related_name='events')
-    history_date = ''
-    type = ['UPDATE', 'CREATE', 'BULK_CREATE', 'BULK_UPDATE', ]
-    # TODO: maybe wrap all create/update methods on queryset
-    #       to get accurate all types
-    trigger = ['save', 'bulk_create', 'bulk_update']  # TODO: move to ManagerEvent
+    """
+    An instance of this always belongs to a ManagerEvent-instance.
+    """
+    trigger = models.CharField(
+        # TODO: add all operations in Atomic-history?
+        choices=(
+            ('QB', 'queryset: bulk_create'),
+            ('QU', 'queryset: update'),
+            ('QD', 'queryset: delete'),
+            ('D', 'delete'),
+            ('CS', 'creation by save'),
+            ('US', 'update by save')
+        )
+    )
+    # TODO: check-constraint that this corresponds to trigger (actually this
+    #       does not need db-column? -> but filtering?)
+    # TODO: also check constraints for before and after concerning this
+    type = models.CharField(
+        choices=(
+            ('C', 'creation'),
+            ('U', 'update'),
+            ('D', 'deletion'),
+        )
+    )
+    # TODO: this must be adjusted to differences-boolean
+    #       -> field is added if True, otherwise not
+    # TODO: if true -> consistency checks
+    diff = None
+    object_history = models.ForeignKey(
+        'history.ObjectHistory', related_name='events', on_delete=models.CASCADE,
+    )
+    history_date = models.DateTimeField(auto_now_add=True)
+
+    before = models.OneToOneField(
+        'history.HistoryBaseModel',
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='next_event',
+    )
+    after = models.OneToOneField(
+        'history.HistoryBaseModel',
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='previous_event',
+    )
+
+    class Meta:
+        # TODO: order_by history_date!
+        order_by = ()
 
     def __str__(self):
         return '{type}-event triggered by '
 
-    # TODO: order_by history_date!
+    # TODO: do we really need this here?
+    @property
+    def history_id(self):
+        return self.object_history.model
 
+    # TODO: do we really need this here?
+    @property
+    def id(self):
+        return self.object_history.model
 
-# TODO: also we need a direct reference to the history_id and so on...
-# TODO: check constraints for before and after
-# TODO: maybe remove this in favor of ObjectEvent -> type is a computed value by before and after -> get control over setattr by type
-# TODO: do check constraints that prevent saving before/after when the type is create/update -> sql
-class Creation(ObjectEvent):
-    # TODO: this should actually have o2o field to object_history with related_name creation
-    after = models.ForeignKey('history.PseudoHistoryModel', related_name='previous_event')
-
-
-class Update(ObjectEvent):
-    # TODO: FK to history instance -> Generic-FK
-    # TODO: next and previous for Event and event and next_event
-    before = models.ForeignKey('history.PseudoHistoryModel', related_name='next_event')
-    after = models.ForeignKey('history.PseudoHistoryModel', related_name='previous_event')
-
-
-class Deletion(ObjectEvent):
-    # TODO: this should actually have o2o field to object_history with related_name deletion
-    before = models.ForeignKey('history.Pseudo...', related_name='next_event')
-
-    # TODO: also o2o field to tracking_connetion to another instance -> maybe in
-    #       the future
+    # TODO: do we really need this here?
+    @property
+    def model(self):
+        return self.object_history.co
