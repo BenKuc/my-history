885570c788c2fedb93e1c77e69224e12b3662cd7
Benedict Kuchenmeister <benedict.kuchenmeister@gmail.com>
2018-08-22 19:54:33 +0200

    empty functions for receivers

--- history/events/receivers.py
+++ history/events/receivers.py
@@ -1,14 +1,18 @@
-def save_receiver(Model, instance, created, raw, using, update_fields):
+def save_receiver(sender, instance, created, raw, using, update_fields):
     pass
 
 
-def update_receiver():
+def update_receiver(sender, instance, using):
     pass
 
 
-def bulk_create_receiver():
+def bulk_create_receiver(sender, objs, batch_size):
     pass
 
 
-def delete_receiver():
+def mt_bulk_create_receiver(sender, objs, batch_size):
+    pass
+
+
+def delete_receiver(sender, instance, using):
     pass
